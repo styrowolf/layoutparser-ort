31886e0b7c9c0a6aba186ae1726584b1506912c0
Oğuz Kurt <kurt.oguz@outlook.com>
2024-04-27 22:27:51 +0300

    added photo for prediction example

--- example/predict.rs
+++ example/predict.rs
@@ -1,7 +1,7 @@
 use layoutparser_ort::{Detectron2ONNXModel, Result};
 
 fn main() -> Result<()> {
-    let img = image::open("../paper-example.png").unwrap();
+    let img = image::open("data/paper-example.png").unwrap();
 
     let model = Detectron2ONNXModel::new(layoutparser_ort::ModelType::FasterRCNN)?;
 
